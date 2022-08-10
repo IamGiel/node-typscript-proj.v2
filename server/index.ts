bcb4503ee45c91bf6f78ae0f537de94c2e89db0e
Alpha Giel De Asis <lfagel84@gmail.com>
2022-08-10 18:03:06 -0400

    run client and server in one script - npm run dev, with concurrently

---
+++ server/index.ts
@@ -0,0 +1,35 @@
+
+console.log("app.ts here! HELLO 👋")
+
+import express, {Request, Response, NextFunction, Application, ErrorRequestHandler} from "express";
+import {Server} from "http";
+import createHttpError from "http-errors";
+import {config} from "dotenv";
+
+config() // this runs dotenv
+
+const PORT:number = Number(process.env.PORT);
+const app:Application = express()
+
+app.get('/', (req:Request,res:Response,next:NextFunction)=> {
+    res.send(`hello 🌏, rootRoute here! 👋"`)
+})
+
+// error handler 
+app.use((req:Request,res:Response,next:NextFunction)=> {
+    next(new createHttpError.NotFound())
+})
+
+const errorHandler:ErrorRequestHandler = (err,req,res,next)=> {
+    res.status(err.status || 500)
+    res.send({
+        status:err.status || 500,
+        message:err.message
+    })
+}
+
+app.use(errorHandler)
+
+const server:Server = app.listen(PORT, ()=> {
+    console.log(`server is 🏃🏃🏃💨 running on port: ${PORT} and react client 🌏 is in port: 3000`)
+})
