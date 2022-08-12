67e06cac18406163b3d9fd50fcd6bcb50bd71199
Alpha Giel De Asis <lfagel84@gmail.com>
2022-08-12 14:36:40 -0400

    postinstall script'

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,11 @@
 import express, {Application, Request, Response, NextFunction} from "express";
+import path from "path";
 const app:Application = express();
 
+app.use(express.static(path.join(__dirname, '../client/build')));
 app.get("/", (req:Request, res:Response, next:NextFunction)=> {
     res.send("👋 hello 🌏 ")
+    
 })
 
 app.listen(3000, ()=> console.log(`server running in PORT 3000`))
