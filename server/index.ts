f98452e2db13c2bd94f5afe7a0b803f3e704216c
Alpha Giel De Asis <lfagel84@gmail.com>
2022-08-12 16:27:40 -0400

    Heroku

--- server/index.ts
+++ server/index.ts
@@ -3,10 +3,12 @@ import path from 'path';
 const __dirname = path.resolve();
 const app:Application = express();
 
+require("dotenv").config();
+
 app.use(express.static(path.join(__dirname, './client/build')));
 app.get("/", (req:Request, res:Response, next:NextFunction)=> {
     res.send("👋 hello 🌏 ")
     
 })
 
-app.listen(3000, ()=> console.log(`server running in PORT 3000`))
+app.listen(process.env.PORT || 3000, ()=> console.log(`server running in PORT 3000`))
