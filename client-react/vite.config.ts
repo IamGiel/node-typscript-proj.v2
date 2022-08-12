6972efdf5ce2814207faba9154789f6f6ccacd07
Alpha Giel De Asis <lfagel84@gmail.com>
2022-08-12 14:50:15 -0400

    depl

--- client-react/vite.config.ts
+++ client-react/vite.config.ts
@@ -3,17 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    extensions: [
-      ".mjs",
-      ".js",
-      ".ts",
-      ".jsx",
-      ".tsx",
-      ".json",
-      ".vue",
-      ".scss",
-    ]
-  }
+  plugins: [react()]
 })
--- dist/index.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-const app = express();
-app.get("/", (req, res, next) => {
-    res.send("👋 hello 🌏 ");
-});
-app.listen(3000, () => console.log(`server running in PORT 3000`));
