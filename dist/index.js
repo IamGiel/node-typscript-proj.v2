6743e00c615227b717248505c89b10c404740830
Alpha Giel De Asis <lfagel84@gmail.com>
2022-08-12 16:30:27 -0400

    Heroku

--- dist/index.js
+++ dist/index.js
@@ -2,8 +2,10 @@ import express from "express";
 import path from 'path';
 const __dirname = path.resolve();
 const app = express();
+import 'dotenv/config'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+console.log("process ev ", process.env.PORT);
 app.use(express.static(path.join(__dirname, './client/build')));
 app.get("/", (req, res, next) => {
     res.send("👋 hello 🌏 ");
 });
-app.listen(3000, () => console.log(`server running in PORT 3000`));
+app.listen(process.env.PORT || 3000, () => console.log(`server running in PORT 3000`));
--- server/index.ts
+++ server/index.ts
@@ -3,8 +3,10 @@ import path from 'path';
 const __dirname = path.resolve();
 const app:Application = express();
 
-require("dotenv").config();
+import 'dotenv/config' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 
+
+console.log("process ev ", process.env.PORT)
 app.use(express.static(path.join(__dirname, './client/build')));
 app.get("/", (req:Request, res:Response, next:NextFunction)=> {
     res.send("👋 hello 🌏 ")
