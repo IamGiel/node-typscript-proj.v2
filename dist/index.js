2b7964e2d4701894b969a55b6b054254a3fad118
Alpha Giel De Asis <lfagel84@gmail.com>
2022-08-12 22:52:50 -0400

    proper node ts and react ts set up with package json scripts

--- dist/index.js
+++ dist/index.js
@@ -2,10 +2,11 @@ import express from "express";
 import path from 'path';
 const __dirname = path.resolve();
 const app = express();
+const PORT = process.env.PORT;
 import 'dotenv/config'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-console.log("process ev ", process.env.PORT);
+console.log("process ev ", PORT);
 app.use(express.static(path.join(__dirname, './client/build')));
 app.get("/", (req, res, next) => {
     res.send("👋 hello 🌏 ");
 });
-app.listen(process.env.PORT || 3000, () => console.log(`server running in PORT 3000`));
+app.listen(PORT, () => console.log(`server running in PORT =>  ${PORT}`));
--- server/index.ts
+++ server/index.ts
@@ -2,15 +2,16 @@ import express, {Application, Request, Response, NextFunction} from "express";
 import path from 'path';
 const __dirname = path.resolve();
 const app:Application = express();
-
+const PORT = process.env.PORT;
 import 'dotenv/config' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 
 
-console.log("process ev ", process.env.PORT)
+console.log("process ev ", PORT)
+
 app.use(express.static(path.join(__dirname, './client/build')));
 app.get("/", (req:Request, res:Response, next:NextFunction)=> {
     res.send("👋 hello 🌏 ")
     
 })
 
-app.listen(process.env.PORT || 3000, ()=> console.log(`server running in PORT 3000`))
+app.listen(PORT, ()=> console.log(`server running in PORT =>  ${PORT}`))
